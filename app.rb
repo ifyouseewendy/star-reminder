1f73cb24b2259da8dabedecb2b594ff3de4a6cfe
Di Wen <ifyouseewendy@gmail.com>
2017-08-07 17:58:31 -0400

    Replace sinatra view with static index html

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require "sinatra/base"
 class MyApp < Sinatra::Base
   set :public_folder, File.dirname(__FILE__) + "/dist"
 
-  get "/" do
-    erb :index, layout: :layout
+  get "/test" do
+    "hello world"
   end
 end
