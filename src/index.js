136a9374b5973d598082b398a8457ee2ce0328bb
Di Wen <ifyouseewendy@gmail.com>
2017-08-07 17:35:01 -0400

    Add babel loader

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 
 window.Application = {
   render: function() {
-    ReactDOM.render("<h1>Hello, world!</h1>", document.getElementById("root"));
+    ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
   },
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,10 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
+    ],
   }
 };
