e3daa29ee648f53c8580377a3c809f9e128fbf32
Di Wen <ifyouseewendy@gmail.com>
2017-08-08 21:07:40 -0400

    Serve index from sinatra

--- app.rb
+++ app.rb
@@ -4,6 +4,10 @@ require "sinatra/base"
 class MyApp < Sinatra::Base
   set :public_folder, File.dirname(__FILE__) + "/dist"
 
+  get "/" do
+    File.read("src/index.html")
+  end
+
   get "/test" do
     "hello world"
   end
