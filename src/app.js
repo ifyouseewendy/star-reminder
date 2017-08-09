234a213359ec1a0e277197adb5aefaa4ca04691e
Di Wen <ifyouseewendy@gmail.com>
2017-08-08 20:30:52 -0400

    Add material ui

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import React from "react";
+import injectTapEventPlugin from "react-tap-event-plugin";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import RaisedButton from "material-ui/RaisedButton";
+
+injectTapEventPlugin();
 
 const App = () => (
-  <p>Hi Rendering from React</p>
+  <MuiThemeProvider>
+    <RaisedButton label="Default" />
+  </MuiThemeProvider>
 );
 
 export default App;
